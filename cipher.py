77b6a98dc09aef62b46293707a69bc27779eb2ab
jesmaail <josephesmaail93@gmail.com>
2016-07-26 12:04:08 +0100

    Completed with minimal functionality

---
+++ cipher.py
@@ -0,0 +1,30 @@
+print("")
+print("--- Caesar Cipher Encryption ---")
+
+# Get Cipher key as integer
+print("")
+shift = int(input("Please enter cipher key: "))
+
+# Get Message as lowercase string
+print("")
+input_str = input("Please enter your message: ").lower()
+
+# Convert string to list of characters
+str_to_char = list(input_str)
+
+# Convert list of characters to list of integers
+char_to_int = [ord(i) for i in str_to_char]
+
+
+# Shift characters, handle values outside boundaries
+for i in range(len(char_to_int)):
+	char_to_int[i] += shift
+	if char_to_int[i] > ord('z'):
+		char_to_int[i] -= 26
+	elif char_to_int[i] < ord('a'):
+		char_to_int[i] += 26
+
+# Convert list of shifted integers to output string
+output_str = "".join([chr(i) for i in char_to_int])
+
+print("Decrypted message: ", output_str)
